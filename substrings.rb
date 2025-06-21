f904ca02557d0f19146852b5fa2c817706e87b79
Henry Mandorla <henry.mandorla@gmail.com>
2025-06-21 14:49:59 +1000

    Substring check method added

--- substrings.rb
+++ substrings.rb
@@ -1,11 +1,19 @@
 def substrings(string, substring)
-    if substring.is_a? == false
+  #check if substring is an array
+    if substring.is_a?(Array) == false
         return "Library of substrings must be an array"
     end
+  #split the string into array of chars
     string_array = string.split("")
-    substring_array = substring.split("")
-    if string_array.each_cons(substring_array.length).include?(substring_array)
-        return true
+    final_hash = {}
+  #loop through the substring array and check the main string if there is a match
+    substring.each do |item|
+      sub_chars = item.split("")
+      if string_array.each_cons(sub_chars.length).include?(sub_chars)
+        sub_word = sub_chars.join("")
+        total_count = string_array.each_cons(sub_chars.length).count(sub_chars)
+        final_hash[sub_word] = total_count
+      end
     end
-        return false
+  return final_hash
 end
